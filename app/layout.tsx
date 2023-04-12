de46b250ead5c6003564822b0cf585fb7e028af4
SkyLight <fll2680@163.com>
2023-04-12 23:32:06 +0800

    修改title

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,10 +7,10 @@ import { getBuildConfig } from "./config/build";
 const buildConfig = getBuildConfig();
 
 export const metadata = {
-  title: "ChatGPT Next Web",
+  title: "亮亮大帅比",
   description: "Your personal ChatGPT Chat Bot.",
   appleWebApp: {
-    title: "ChatGPT Next Web",
+    title: "亮亮的AI",
     statusBarStyle: "default",
   },
   themeColor: "#fafafa",
